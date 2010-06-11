67e74fca8ee1dbeb161d512a33a4aa21bc0ba72e
Arnaud Lacombe <lacombar@gmail.com>
2010-06-11 12:43:22 -0400

    compat/2.6.18: add compat skeleton for the `hwrng' API
    Signed-off-by: Arnaud Lacombe <lacombar@gmail.com>

--- include/linux/compat-2.6.18.h
+++ include/linux/compat-2.6.18.h
@@ -8,6 +8,30 @@
 
 #define roundup(x, y)	((((x) + ((y) - 1)) / (y)) * (y))
 
+struct hwrng
+{
+	const char *name;
+	int (*init)(struct hwrng *rng);
+	void (*cleanup)(struct hwrng *rng);
+	int (*data_present)(struct hwrng *rng);
+	int (*data_read)(struct hwrng *rng, u32 *data);
+	unsigned long priv;
+
+	/* internal. */
+	struct list_head list;
+};
+
+static inline int
+hwrng_register(struct hwrng *rng)
+{
+	return -ENOSYS;
+}
+
+static void
+hwrng_unregister(struct hwrng *rng)
+{
+}
+
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,18)) */
 
 #endif /* LINUX_26_18_COMPAT_H */
