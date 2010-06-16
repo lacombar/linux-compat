e6087383701a0810577c67f1dd3a12843084af6c
Arnaud Lacombe <lacombar@gmail.com>
2010-06-16 19:48:26 -0400

    compat/2.6.17: add compat macro for `NET_SKB_PAD'
    Signed-off-by: Arnaud Lacombe <lacombar@gmail.com>

--- include/linux/compat-2.6.17.h
+++ include/linux/compat-2.6.17.h
@@ -6,6 +6,8 @@
 /* Compat work for 2.6.17 */
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,17))
 
+#define NET_SKB_PAD	16
+
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,17)) */
 
 #endif /* LINUX_26_17_COMPAT_H */
