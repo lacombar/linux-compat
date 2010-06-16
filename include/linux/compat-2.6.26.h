33e94ef49c23b9a76e40d2b0516b7e57c46b79ab
Arnaud Lacombe <lacombar@gmail.com>
2010-06-16 17:48:44 -0400

    compat/2.6.26: limit namespace pollution
    Signed-off-by: Arnaud Lacombe <lacombar@gmail.com>

--- include/linux/compat-2.6.26.h
+++ include/linux/compat-2.6.26.h
@@ -5,20 +5,7 @@
 
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,26))
 
-#include <linux/device.h>
-#include <linux/list.h>
-#include <linux/kernel.h>
-#include <linux/jiffies.h>
-#include <net/sock.h>
-#include <linux/fs.h>
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,24))
-#include <net/net_namespace.h>
-#endif
-#include <linux/fs.h>
-#include <linux/types.h>
-
-/* These jiffie helpers added as of 2.6.26 */
-
+#ifdef _LINUX_JIFFIES_H	/* <linux/jiffies.h> */
 /*
  * These four macros compare jiffies and 'a' for convenience.
  */
@@ -34,14 +21,32 @@
 
 /* time_is_after_eq_jiffies(a) return true if a is after or equal to jiffies*/
 #define time_is_after_eq_jiffies(a) time_before_eq(jiffies, a)
+#endif /* _LINUX_JIFFIES_H */
 
-/* This comes from include/linux/input.h */
+#ifdef _INPUT_H	/* <linux/input.h> */
 #define SW_RFKILL_ALL           0x03  /* rfkill master switch, type "any"
 					 set = radio enabled */
+#endif
 
+#ifdef _DEVICE_H_	/* <linux/device.h> */
 extern int dev_set_name(struct device *dev, const char *name, ...)
 			__attribute__((format(printf, 2, 3)));
 
+extern struct device *device_create_drvdata(struct class *cls,
+	struct device *parent,
+	dev_t devt,
+	void *drvdata,
+	const char *fmt, ...)
+__attribute__((format(printf, 5, 6)));
+
+static inline const char *dev_name(struct device *dev)
+{
+	/* will be changed into kobject_name(&dev->kobj) in the near future */
+	return dev->bus_id;
+}
+#endif
+
+#ifdef _LINUX_KERNEL_H	/* <linux/kernel.h> */
 /**
  * clamp - return a value clamped to a given range with strict typechecking
  * @val: current value
@@ -77,36 +82,6 @@ extern int dev_set_name(struct device *dev, const char *name, ...)
 	__val = __val < __min ? __min: __val;	\
 	__val > __max ? __max: __val; })
 
-
-/* from include/linux/device.h */
-/* device_create_drvdata() is new */
-extern struct device *device_create_drvdata(struct class *cls,
-	struct device *parent,
-	dev_t devt,
-	void *drvdata,
-	const char *fmt, ...)
-__attribute__((format(printf, 5, 6)));
-
-/* This is from include/linux/list.h */
-
-/**
- * list_is_singular - tests whether a list has just one entry.
- * @head: the list to test.
- */
-static inline int list_is_singular(const struct list_head *head)
-{
-	return !list_empty(head) && (head->next == head->prev);
-}
-
-/* This is from include/linux/device.h, which was added as of 2.6.26 */
-static inline const char *dev_name(struct device *dev)
-{
-	/* will be changed into kobject_name(&dev->kobj) in the near future */
-	return dev->bus_id;
-}
-
-/* This is from include/linux/kernel.h, which was added as of 2.6.26 */
-
 /**
  * clamp_val - return a value clamped to a given range using val's type
  * @val: current value
@@ -118,16 +93,26 @@ static inline const char *dev_name(struct device *dev)
  * type and min and max are literals that will otherwise be assigned a signed
  * integer type.
  */
-
 #define clamp_val(val, min, max) ({             \
 	typeof(val) __val = (val);              \
 	typeof(val) __min = (min);              \
 	typeof(val) __max = (max);              \
 	__val = __val < __min ? __min: __val;   \
 	__val > __max ? __max: __val; })
+#endif /* _LINUX_KERNEL_H */
 
-/* This comes from include/net/net_namespace.h */
+#ifdef _LINUX_LIST_H	/* <linux/list.h> */
+/**
+ * list_is_singular - tests whether a list has just one entry.
+ * @head: the list to test.
+ */
+static inline int list_is_singular(const struct list_head *head)
+{
+	return !list_empty(head) && (head->next == head->prev);
+}
+#endif
 
+#ifdef __NET_NET_NAMESPACE_H	/* <net/net_namespace.h> */
 #ifdef CONFIG_NET_NS
 static inline
 int net_eq(const struct net *net1, const struct net *net2)
@@ -141,16 +126,9 @@ int net_eq(const struct net *net1, const struct net *net2)
 	return 1;
 }
 #endif
+#endif /* __NET_NET_NAMESPACE_H */
 
-static inline
-void dev_net_set(struct net_device *dev, struct net *net)
-{
-#ifdef CONFIG_NET_NS
-	release_net(dev->nd_net);
-	dev->nd_net = hold_net(net);
-#endif
-}
-
+#ifdef _SOCK_H	/* <net/sock.h> */
 static inline
 struct net *sock_net(const struct sock *sk)
 {
@@ -160,12 +138,9 @@ struct net *sock_net(const struct sock *sk)
 	return &init_net;
 #endif
 }
+#endif
 
-/* This comes from include/linux/netdevice.h */
-
-/*
- * Net namespace inlines
- */
+#ifdef _LINUX_NETDEVICE_H	/* <linux/netdevice.h> */
 static inline
 struct net *dev_net(const struct net_device *dev)
 {
@@ -181,6 +156,17 @@ struct net *dev_net(const struct net_device *dev)
 #endif
 }
 
+static inline
+void dev_net_set(struct net_device *dev, struct net *net)
+{
+#ifdef CONFIG_NET_NS
+	release_net(dev->nd_net);
+	dev->nd_net = hold_net(net);
+#endif
+}
+#endif
+
+#ifdef LINUX_ASPM_H		/* <linux/pci-aspm.h> */
 #define PCIE_LINK_STATE_L0S    1
 #define PCIE_LINK_STATE_L1     2
 #define PCIE_LINK_STATE_CLKPM  4
@@ -188,7 +174,7 @@ struct net *dev_net(const struct net_device *dev)
 static inline void pci_disable_link_state(struct pci_dev *pdev, int state)
 {
 }
-/* source: include/linux/pci-aspm.h */
+#endif
 
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,26)) */
 
