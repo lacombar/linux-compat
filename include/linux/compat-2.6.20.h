8b5acddfdec0702b3b32a9e1cc0220d68f406cf4
Arnaud Lacombe <lacombar@gmail.com>
2010-06-16 18:04:33 -0400

    compat/2.6.20: add compat macro for `struct delayed_work'
    Signed-off-by: Arnaud Lacombe <lacombar@gmail.com>

--- include/linux/compat-2.6.20.h
+++ include/linux/compat-2.6.20.h
@@ -6,6 +6,9 @@
 /* Compat work for 2.6.20 */
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,20))
 
+/* `struct work_struct' and `struct delayed_work' are field compatible */
+#define delayed_work work_struct
+
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,20)) */
 
 #endif /* LINUX_26_20_COMPAT_H */
