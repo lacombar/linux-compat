29539c217202f7d98056a42d292234ad21a6b97b
Arnaud Lacombe <lacombar@gmail.com>
2010-06-16 19:23:19 -0400

    compat/2.6.20: limit namespace pollution
    Signed-off-by: Arnaud Lacombe <lacombar@gmail.com>

--- include/linux/compat-2.6.20.h
+++ include/linux/compat-2.6.20.h
@@ -3,11 +3,13 @@
 
 #include <linux/version.h>
 
-/* Compat work for 2.6.20 */
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,20))
 
+/* <linux/workqueue.h> */
+#ifdef _LINUX_WORKQUEUE_H
 /* `struct work_struct' and `struct delayed_work' are field compatible */
 #define delayed_work work_struct
+#endif
 
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,20)) */
 
