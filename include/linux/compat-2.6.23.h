0c69591a46ac1b7ddccac934cc34391a22f7250e
Arnaud Lacombe <lacombar@gmail.com>
2010-06-16 19:26:30 -0400

    compat/2.6.23: limit namespace pollution
    Signed-off-by: Arnaud Lacombe <lacombar@gmail.com>

--- include/linux/compat-2.6.23.h
+++ include/linux/compat-2.6.23.h
@@ -3,16 +3,10 @@
 
 #include <linux/version.h>
 
-/* Compat work for < 2.6.23 */
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,23))
 
-#include <linux/netdevice.h>
-#include <linux/notifier.h>
-#include <linux/sched.h>
-#include <linux/workqueue.h>
-#include <linux/genetlink.h>
-#include <net/sch_generic.h>
-
+/* <linux/notifier.h> */
+#ifdef _LINUX_NOTIFIER_H
 /* Encapsulate (negative) errno value (in particular, NOTIFY_BAD <=> EPERM). */
 static inline int notifier_from_errno(int err)
 {
@@ -25,12 +19,10 @@ static inline int notifier_to_errno(int ret)
 	ret &= ~NOTIFY_STOP_MASK;
 	return ret > NOTIFY_OK ? NOTIFY_OK - ret : 0;
 }
+#endif
 
-/*
- * Tell gcc if a function is cold. The compiler will assume any path
- * directly leading to the call is unlikely.
- */
-
+/* <linux/compiler.h> */
+#ifdef __LINUX_COMPILER_H
 #if !(__GNUC__ == 4 && __GNUC_MINOR__ < 3)
 /* Mark functions as cold. gcc will assume any path leading to a call
  * to them will be unlikely.  This means a lot of manual unlikely()s
@@ -51,17 +43,32 @@ static inline int notifier_to_errno(int ret)
 #ifndef __cold
 #define __cold
 #endif
+#endif /* __LINUX_COMPILER_H */
 
-/* Added as of 2.6.23 in include/linux/netdevice.h */
+/* <linux/netdevice.h> */
+#ifdef _LINUX_NETDEVICE_H
 #define alloc_netdev_mq(sizeof_priv, name, setup, queue) \
 	alloc_netdev(sizeof_priv, name, setup)
 #define NETIF_F_MULTI_QUEUE 16384
 
-/* Added as of 2.6.23 on include/linux/netdevice.h */
 static inline int netif_is_multiqueue(const struct net_device *dev)
 {
 	return (!!(NETIF_F_MULTI_QUEUE & dev->features));
 }
+
+/* dev_mc_list was replaced with dev_addr_list, only new member
+   added is da_synced. */
+#define dev_addr_list	dev_mc_list
+#define da_addr		dmi_addr
+#define da_addrlen	dmi_addrlen
+#define da_users	dmi_users
+#define da_gusers	dmi_gusers
+
+extern void	dev_mc_unsync(struct net_device *to, struct net_device *from);
+extern int	dev_mc_sync(struct net_device *to, struct net_device *from);
+
+extern void	__dev_set_rx_mode(struct net_device *dev);
+#endif /* _LINUX_NETDEVICE_H */
 
 /* 2.6.23 fixed a bug in tcf_destroy_chain and the parameter changed */
 static inline void tcf_destroy_chain_compat(struct tcf_proto **fl)
@@ -76,33 +83,23 @@ static inline void tcf_destroy_chain_compat(struct tcf_proto **fl)
 	}
 }
 
-/* dev_mc_list was replaced with dev_addr_list as of 2.6.23,
- * only new member added is da_synced. */
-#define dev_addr_list	dev_mc_list
-#define da_addr		dmi_addr
-#define da_addrlen	dmi_addrlen
-#define da_users	dmi_users
-#define da_gusers	dmi_gusers
-
-/* Our own 2.6.22 port on compat.c */
-extern void	dev_mc_unsync(struct net_device *to, struct net_device *from);
-extern int	dev_mc_sync(struct net_device *to, struct net_device *from);
-
-/* Our own 2.6.22 port on compat.c */
-extern void	__dev_set_rx_mode(struct net_device *dev);
-
 /* Simple to add this */
 extern int cancel_delayed_work_sync(struct delayed_work *work);
 
 #define cancel_delayed_work_sync cancel_rearming_delayed_work
 
+/* <linux/debugfs.h> */
+#ifdef _DEBUGFS_H_
 #define debugfs_rename(a, b, c, d) 1
+#endif
 
 /* nl80211 requires multicast group support which is new and added on
  * 2.6.23. We can't add support for it for older kernels to support it
  * genl_family structure was changed. Lets just let through the
  * genl_register_mc_group call. This means no multicast group suppport */
 
+/* <net/genetlink.h> */
+#ifdef __NET_GENERIC_NETLINK_H
 #define genl_register_mc_group(a, b) 0
 
 /**
@@ -120,11 +117,15 @@ struct genl_multicast_group
 	char                    name[GENL_NAMSIZ];
 	u32                     id;
 };
+#endif /* __NET_GENERIC_NETLINK_H */
 
-
-/* Added as of 2.6.23 */
+/* <linux/pci.h> */
+#ifdef LINUX_PCI_H
 int pci_try_set_mwi(struct pci_dev *dev);
+#endif
 
+/* <linux/freezer.h> */
+#ifdef FREEZER_H_INCLUDED
 /* Added as of 2.6.23 */
 #ifdef CONFIG_PM_SLEEP
 /*
@@ -138,6 +139,7 @@ static inline void set_freezable(void)
 #else
 static inline void set_freezable(void) {}
 #endif /* CONFIG_PM_SLEEP */
+#endif /* FREEZER_H_INCLUDED */
 
 #else
 #define tcf_destroy_chain_compat tcf_destroy_chain
