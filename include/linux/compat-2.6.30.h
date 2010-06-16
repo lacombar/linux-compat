367f06dea1050a61c9c4ae0bbc1889510770483c
Arnaud Lacombe <lacombar@gmail.com>
2010-06-16 19:40:17 -0400

    compat/2.6.30: limit namespace pollution
    Signed-off-by: Arnaud Lacombe <lacombar@gmail.com>

--- include/linux/compat-2.6.30.h
+++ include/linux/compat-2.6.30.h
@@ -5,18 +5,23 @@
 
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,30))
 
-#include <linux/device.h>
-
+/* <linux/tracepoint.h> */
+#ifdef _LINUX_TRACEPOINT_H
 #ifndef TP_PROTO
 #define TP_PROTO(args...)	TPPROTO(args)
 #endif
 #ifndef TP_ARGS
 #define TP_ARGS(args...)	TPARGS(args)
 #endif
+#endif
 
+/* <linux/interrupt.h> */
+#ifdef _LINUX_INTERRUPT_H
 #define IRQ_WAKE_THREAD	(2)
+#endif
 
-/* From : include/linux/pm.h */
+/* <linux/pm.h> */
+#ifdef _LINUX_PM_H
 /* How to reorder dpm_list after device_move() */
 enum dpm_order {
 	DPM_ORDER_NONE,
@@ -24,13 +29,17 @@ enum dpm_order {
 	DPM_ORDER_PARENT_BEFORE_DEV,
 	DPM_ORDER_DEV_LAST,
 };
+#endif
 
+/* <linux/device.h> */
+#ifdef _DEVICE_H_
 static inline void dev_set_uevent_suppress(struct device *dev, int val)
 {
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,21))
 	dev->uevent_suppress = val;
 #endif
 }
+#endif
 
 /*
  * Print a one-time message (analogous to WARN_ONCE() et al):
