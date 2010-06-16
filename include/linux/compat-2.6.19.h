f43e96c78d9460bf61ee7760330f28a17d8aacdc
Arnaud Lacombe <lacombar@gmail.com>
2010-06-16 19:40:53 -0400

    compat/2.6.19: limit namespace pollution
    Signed-off-by: Arnaud Lacombe <lacombar@gmail.com>

--- include/linux/compat-2.6.19.h
+++ include/linux/compat-2.6.19.h
@@ -6,17 +6,22 @@
 /* Compat work for 2.6.19 */
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,19))
 
-#include <linux/slab.h>
-
+/* <linux/slab.h> */
+#ifdef _LINUX_SLAB_H
 #define kmem_cache_destroy(cachep)	({ kmem_cache_destroy(cachep); 0; })
+#endif
 
 #else /* (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,19)) */
 
 typedef _Bool			bool;
+
+/* <linux/stddef.h> */
+#ifdef _LINUX_STDDEF_H
 enum {
 	false = 0,
 	true = 1
 };
+#endif
 
 #endif /* (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,19)) */
 
