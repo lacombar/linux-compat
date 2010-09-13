ce8437d8265335f8c5261dd570732d7e328aa4f1
Arnaud Lacombe <lacombar@gmail.com>
2010-09-13 12:06:26 -0400

    compat/2.6.17: limit namespace pollution
    Signed-off-by: Arnaud Lacombe <lacombar@gmail.com>

--- include/linux/compat-2.6.17.h
+++ include/linux/compat-2.6.17.h
@@ -6,7 +6,10 @@
 /* Compat work for 2.6.17 */
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,17))
 
+/* <linux/skbuff.h> */
+#ifdef _LINUX_SKBUFF_H
 #define NET_SKB_PAD	16
+#endif
 
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,17)) */
 
