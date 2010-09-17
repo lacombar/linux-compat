7266529a4442097e21af773f837637d1d0e888f0
Arnaud Lacombe <lacombar@gmail.com>
2010-09-17 17:38:19 -0400

    compat/2.6.17: add header's skeleton
    Signed-off-by: Arnaud Lacombe <lacombar@gmail.com>

---
+++ include/linux/compat-2.6.17.h
@@ -0,0 +1,11 @@
+#ifndef LINUX_26_17_COMPAT_H
+#define LINUX_26_17_COMPAT_H
+
+#include <linux/version.h>
+
+/* Compat work for 2.6.17 */
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,17))
+
+#endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,17)) */
+
+#endif /* LINUX_26_17_COMPAT_H */
