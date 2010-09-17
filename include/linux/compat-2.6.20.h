7b7ec4c504843365ea404b3564b5cd6500a1fdc8
Arnaud Lacombe <lacombar@gmail.com>
2010-09-17 17:41:07 -0400

    compat/2.6.20: add header's skeleton
    Signed-off-by: Arnaud Lacombe <lacombar@gmail.com>

---
+++ include/linux/compat-2.6.20.h
@@ -0,0 +1,11 @@
+#ifndef LINUX_26_20_COMPAT_H
+#define LINUX_26_20_COMPAT_H
+
+#include <linux/version.h>
+
+/* Compat work for 2.6.20 */
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,20))
+
+#endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,20)) */
+
+#endif /* LINUX_26_20_COMPAT_H */
