68354408b2be213ebea8f6378bdb6c9aac4fba74
Arnaud Lacombe <lacombar@gmail.com>
2010-10-01 17:43:23 -0400

    compat/2.6.22: add compat inline for ip_hdrlen()
    Signed-off-by: Arnaud Lacombe <lacombar@gmail.com>

--- include/linux/compat-2.6.22.h
+++ include/linux/compat-2.6.22.h
@@ -85,6 +85,11 @@ static inline struct iphdr *ip_hdr(const struct sk_buff *skb)
 	return (struct iphdr *)skb_network_header(skb);
 }
 
+static inline unsigned int ip_hdrlen(const struct sk_buff *skb)
+{
+	return ip_hdr(skb)->ihl * 4;
+}
+
 static inline struct tcphdr *tcp_hdr(const struct sk_buff *skb)
 {
 	return (struct tcphdr *)skb_transport_header(skb);
